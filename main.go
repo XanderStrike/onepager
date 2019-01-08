db3c4884221ff6b1f3ced9fc408e06229343de6b
Alexander Standke <xanderstrike@gmail.com>
2019-01-07 17:46:33 -0800

    init

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/peterbourgon/diskv"
+)
+
+type HomePage struct {
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	render(w, "index.html", HomePage{})
+}
+
+func flatTransform(s string) []string { return []string{} }
+func NewHandler(w http.ResponseWriter, r *http.Request) {
+	filename := fmt.Sprintf("%s.html", r.FormValue("filename"))
+	content := r.FormValue("content")
+	d := diskv.New(diskv.Options{
+		BasePath:     "pages",
+		Transform:    flatTransform,
+		CacheSizeMax: 1024 * 1024,
+	})
+	d.Write(filename, []byte(content))
+	io.WriteString(w, fmt.Sprintf("filename: %s\ncontents: %s", filename, content))
+	log.Println("Saved", filename)
+}
+
+func main() {
+	log.Println("Starting!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/new", NewHandler)
+	r.PathPrefix("/pages").Handler(http.FileServer(http.Dir("./")))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", r))
+}
+
+func render(w http.ResponseWriter, filename string, data interface{}) {
+	tmpl, err := template.ParseFiles(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if err := tmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
